e5d033e06a7552cda55ae571dcf68091f09fde91
ljw8161 <45479186+ljw8161@users.noreply.github.com>
2020-03-27 20:47:51 +0900

    Update Floating Point Adder cpp
    불필요한 코드 삭제 및 주석 추가

--- FP.cpp
+++ FP.cpp
@@ -23,68 +23,6 @@ std::uniform_int_distribution<int>  RandomExponent(0, 254);
 std::uniform_int_distribution<int>  RandomMantisa(0, 0x7FFFFF);
 std::uniform_int_distribution<int>  RandomSign(0, 1);
 
-//std::uniform_int_distribution<int>  DRandomExponent(0, 0x7FF);
-//std::uniform_int_distribution<int>  DRandomMantisa(0, 0x7FFFFFFFFFFFF);
-//std::uniform_int_distribution<int>  DRandomSign(0, 1);
-
-
-
-/*
-void
-print_float_bits(float f)
-{
-int i;
-
-i = FLOATBITS - 1;
-PUT_BIT(f, i);
-putchar(' ');
-for (i--; i >= 23; i--) {
-PUT_BIT(f, i);
-}
-putchar(' ');
-for (; i >= 0; i--) {
-PUT_BIT(f, i);
-}
-}
-
-int
-main(int argc, char** argv)
-{
-float f;
-
-while (scanf("%f", &f) == 1) {
-printf("%10g = %24.17g = ", f, f);
-print_float_bits(f);
-putchar('\n');
-}
-
-return 0;
-}
-*/
-
-// 비트연산하는것 
-/*void getSME(int*, int*, int*, float);
-
-void getSME(int *s, int *m, int *e, float number)
-{
-unsigned int* ptr = (unsigned int*)& number;
-
-*s = *ptr >> 31;
-*e = *ptr & 0x7f800000;
-*e >>= 23;
-*m = *ptr & 0x007fffff;
-}
-
-void main() {
-printf("%a", (float)0.75);
-int Exponent, Fraction, Sign;
-float num = 0.75;
-
-//Exponent = num & E;
-//Fraction = num & F;
-//Sign = num >> 31 & 1;
-}*/
-
 //Union 사용
 typedef union {
 	float f;
@@ -95,17 +33,7 @@ typedef union {
 	} parts;
 } float_cast;
 
-//typedef union {
-//	double f;
-//	struct {
-//		unsigned int mantisa : 52;
-//		unsigned int exponent : 11;
-//		unsigned int sign : 1;
-//	} parts;
-//} double_cast;
-
 float_cast makeFP();
-//double_cast makeDFP();
 float_cast FPAdder(float_cast a, float_cast b);
 void FPAddition();
 
@@ -120,16 +48,6 @@ float_cast makeFP() {
 	num.parts.sign = 0;
 	return num;
 }
-//double_cast makeDFP() {
-//	//exponet => 127(7F) ~ -128(80)
-//	//mantisa => 524287(7FFFF) ~ 0(0)
-//	double_cast num;
-//	num.parts.exponent = DRandomExponent(generator); //0~255 -> 2^(exponent - 126) * 1.(mantisa)
-//	//if exponent == 255 -> inf
-//	num.parts.mantisa = DRandomMantisa(generator);
-//	num.parts.sign = 0;	
-//	return num;
-//}
 
 float_cast FPAdder(float_cast a, float_cast b) {
 
@@ -218,95 +136,27 @@ float_cast FPAdder(float_cast a, float_cast b) {
 	return z;
 }
 
-void FPAddition() {
-	float_cast a, b, ans, d1;
-	a = makeFP();
-	b = makeFP();
-	printf("--------------A---------------\n");
-	printf("sign = %x\n", a.parts.sign);
-	printf("exponent = %x\n", a.parts.exponent);
-	printf("mantisa = %x\n", a.parts.mantisa);
-	printf("FP = %e\n", a.f);
-	printf("FP = %lf\n", a.f);
-
-	printf("--------------B---------------\n");
-	printf("sign = %x\n", b.parts.sign);
-	printf("exponent = %x\n", b.parts.exponent);
-	printf("mantisa = %x\n", b.parts.mantisa);
-	printf("answer = %e\n", b.f);
-	printf("FP = %lf\n", b.f);
-
-
-	ans = FPAdder(a, b);
-
-	printf("--------------Ans---------------\n");
-	printf("sign = %x\n", ans.parts.sign);
-	printf("exponent = %x\n", ans.parts.exponent);
-	printf("mantisa = %x\n", ans.parts.mantisa);
-	printf("answer = %e\n", ans.f);
-	printf("FP = %lf\n", ans.f);
-
-}
-void compare() {
+int main(void) {
 	float_cast A, B, ans;
 	float_cast orgAns;
 	int cnt = 0;
 	printf("A\t\t+\t\tB\t=\torgANS\t\tmyANS\n");
 	printf("**********************************************************************\n");
-	//for (int i = 0; i < 1000; i++) {
+
 	A = makeFP();
 	B = makeFP();
-	//A.f = 1.196897e+21;
-	//B.f = 6.368948e+23;
+	//A, B 랜덤 지정
 
-	//A.f = 7.899285e-20;
-	//B.f = 8.390388e-22;
-	A.f = 2.494615e+09;
-	B.f = 1.094270e+04;
-	//A.f = 1.626195e+35;
-	//B.f = 1.484308e+34;
+	//A.f = 2.494615e+09;
+	//B.f = 1.094270e+04;
+	//A, B 직접 지정
+
 	ans = FPAdder(A, B);
 	orgAns.f = A.f + B.f;
-	if (orgAns.f != ans.f) {
-		printf("%e    +    %e    =    %e,   %e\n", A.f, B.f, orgAns.f, ans.f);
-		cnt++;
-	}
-	//}
-	printf("\n\n*************%d", cnt);
 
-}
+	printf("%e    +    %e    =    %e,   %e\n", A.f, B.f, orgAns.f, ans.f);
 
-
-
-int main(void) {
-	float_cast d1, d2, ans;// = { .f = 0.75 };
-						   //d1.parts.sign = 0;
-						   //d1.parts.exponent = 0x47f;
-						   //d1.parts.mantisa = 0x731c8;
-						   //d1.parts.sign = 1;
-						   //d1.parts.exponent = 0x7e;
-						   //d1.parts.mantisa = 0x0;
-						   //d1.f = -0.5;
-						   ////d2.f = -0.4375;
-						   ////exponet => 127(7F) ~ -128(80)
-						   ////mantisa => 524287(7FFFF) ~ 0(0)
-						   //printf("sign = %f\n", d1.f);
-
-						   //printf("sign = %x\n", d1.parts.sign);
-						   //printf("exponent = %x\n", d1.parts.exponent);
-						   //printf("mantisa = %x\n", d1.parts.mantisa);
-
-
-						   /*for (int i = 0; i < 100; i++) {
-						   float_cast num = makeFP();
-						   printf("%d %d %d\n", num.parts.exponent, num.parts.mantisa, num.parts.sign);
-						   }*/
-
-						   //FPAddition();
-	compare();
-
-	//RandomGen();
-	//ans.f = FPAdder(d1, d2);
+	printf("\n\n******************************");
 }
 /*
 sign = 1
