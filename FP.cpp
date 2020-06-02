ea96282dd6764137211564262dbc7c0f2f830204
W_Jeehyun <33047795+wjh51333@users.noreply.github.com>
2020-06-02 14:32:35 +0900

    Update FP.cpp

--- FP.cpp
+++ FP.cpp
@@ -55,46 +55,6 @@ float_cast makeFP() {
 	num.parts.sign = RandomSign(generator);
 	return num;
 }
-unsigned int LOA(unsigned int a, unsigned int b)
-{
-	unsigned int m, n, sum;
-	int carry;
-
-	m = a & mask;
-	n = b & mask;
-
-	sum = m | n;
-	carry = (m >> bitnum - 1) & (n >> bitnum - 1);
-
-	sum += (a - m) + (b - n) + (carry << bitnum);
-
-	return sum;
-}
-
-unsigned int ETA1(unsigned int a, unsigned int b)
-{
-	unsigned int M, N, m, n, inaccuratePart = 0, r, sum = 0;
-	int carry, imask = 0x800;
-
-	M = a & mask;
-	N = b & mask;
-
-	while (1) {
-		m = a & imask;
-		n = b & imask;
-		r = m ^ n;
-		inaccuratePart += r;
-		if (m == imask && n == imask) {
-			inaccuratePart += imask - 1;
-			break;
-		}
-		imask /= 2;
-	}
-
-	sum = (a - M) + (b - N) + inaccuratePart;
-
-	return sum;
-}
 
 void extbit_cal(float_cast x, int subEx, int *e)
 {
@@ -220,8 +180,8 @@ unsigned int sum_cal(float_cast &z, float_cast x, float_cast y, int *e)
 
 	return sum;
 }
-float_cast FPAdder(float_cast a, float_cast b) {
 
+float_cast FPAdder(float_cast a, float_cast b) {
 
 	//먼저 두 값이 real number인지 판단해야한다. (inf, -inf, 0, -0, NAN)
 	//0 FF 000000 -> inf, 1 FF 000000 -> -inf, 00000 -> 0, 100000 -> -0
@@ -403,6 +363,47 @@ float_cast FPAdder(float_cast a, float_cast b) {
 	return z;
 }
 
+unsigned int LOA(unsigned int a, unsigned int b)
+{
+	unsigned int m, n, sum;
+	int carry;
+
+	m = a & mask;
+	n = b & mask;
+
+	sum = m | n;
+	carry = (m >> bitnum - 1) & (n >> bitnum - 1);
+
+	sum += (a - m) + (b - n) + (carry << bitnum);
+
+	return sum;
+}
+
+unsigned int ETA1(unsigned int a, unsigned int b)
+{
+	unsigned int M, N, m, n, inaccuratePart = 0, r, sum = 0;
+	int carry, imask = 0x800;
+
+	M = a & mask;
+	N = b & mask;
+
+	while (1) {
+		m = a & imask;
+		n = b & imask;
+		r = m ^ n;
+		inaccuratePart += r;
+		if (m == imask && n == imask) {
+			inaccuratePart += imask - 1;
+			break;
+		}
+		imask /= 2;
+	}
+
+	sum = (a - M) + (b - N) + inaccuratePart;
+
+	return sum;
+}
+
 float_cast AppAdder(float_cast a, float_cast b, int caseNum) {
 
 	//먼저 두 값이 real number인지 판단해야한다. (inf, -inf, 0, -0, NAN)
@@ -430,14 +431,17 @@ float_cast AppAdder(float_cast a, float_cast b, int caseNum) {
 	float_cast z; //return 값
 	z.parts.sign = 0;
 	unsigned int sum = 0;
+	
 	switch (caseNum) {
 	case 1: //LOA
 		sum = LOA(a.parts.mantissa, b.parts.mantissa);
 	case 2: //ETA1
 		sum = ETA1(a.parts.mantissa, b.parts.mantissa);
 	}
+	
 	unsigned int aMantissa_extra;
 	unsigned int bMantissa_extra;
+	
 	aMantissa_extra = a.parts.mantissa - a.parts.mantissa & mask;
 	bMantissa_extra = b.parts.mantissa - b.parts.mantissa & mask;
 	z.parts.mantissa = aMantissa_extra & bMantissa_extra + sum;
@@ -468,6 +472,7 @@ int main(void) {
 		A = makeFP();
 		B = makeFP();
 		orgAns.f = A.f + B.f;
+		
 		ans = FPAdder(A, B);
 		loa = AppAdder(A, B, 1);
 		eta1 = AppAdder(A, B, 2);
